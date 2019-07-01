578cc799b05e35bf966b1f7d259ae78d64238eb8
S.Konishi <s.konishi@magellan.jp>
2019-07-01 16:57:43 +0900

    Dev91. Refactoring...

--- ArduCopter/mode_star.cpp
+++ ArduCopter/mode_star.cpp
@@ -171,226 +171,208 @@ bool ModeStar::setup_star_wp()
 	if (!ret) {
 		fprintf(stderr,"[%s:%d] Leaving w/ return false. WP could not be set...\n",
 				__FUNCTION__, __LINE__);
-		return false;
-	}
+        return false;
+    }
 
-	origin = wp_nav->get_wp_origin();
-	Vector3f wp = wp_nav->get_wp_destination();
-	fprintf(stderr,"[%s:%d] Next: (%f, %f, %f) -> (%f, %f, %f)\n",
-			__FUNCTION__, __LINE__, origin.x, origin.y, origin.z, wp.x, wp.y, wp.z);
+    origin = wp_nav->get_wp_origin();
+    Vector3f wp = wp_nav->get_wp_destination();
+    fprintf(stderr,"[%s:%d] Next: (%f, %f, %f) -> (%f, %f, %f)\n",
+            __FUNCTION__, __LINE__, origin.x, origin.y, origin.z, wp.x, wp.y, wp.z);
 
-	return true;
+    return true;
 }
 
 // do_guided - start guided mode
 bool ModeStar::do_guided(const AP_Mission::Mission_Command& cmd)
 {
-	// only process guided waypoint if we are in guided mode
-	if (copter.control_mode != GUIDED && !(copter.control_mode == AUTO && mode() == Auto_NavGuided)) {
-		return false;
-	}
+    // only process guided waypoint if we are in guided mode
+    if (copter.control_mode != GUIDED && !(copter.control_mode == AUTO && mode() == Auto_NavGuided)) {
+        return false;
+    }
 
-	// switch to handle different commands
-	switch (cmd.id) {
+    // switch to handle different commands
+    switch (cmd.id) {
 
-		case MAV_CMD_NAV_WAYPOINT:
-			{
-				// set wp_nav's destination
-				Location dest(cmd.content.location);
-				return copter.mode_guided.set_destination(dest);
-			}
+        case MAV_CMD_NAV_WAYPOINT:
+            {
+                // set wp_nav's destination
+                Location dest(cmd.content.location);
+                return copter.mode_guided.set_destination(dest);
+            }
 
-		case MAV_CMD_CONDITION_YAW:
-			do_yaw(cmd);
-			return true;
+        case MAV_CMD_CONDITION_YAW:
+            do_yaw(cmd);
+            return true;
 
-		default:
-			// reject unrecognised command
-			return false;
-	}
+        default:
+            // reject unrecognised command
+            return false;
+    }
 
-	return true;
+    return true;
 }
 
 // auto_rtl_start - initialises RTL in AUTO flight mode
 void ModeStar::rtl_start()
 {
-	_mode = Auto_RTL;
+    _mode = Auto_RTL;
 
-	// call regular rtl flight mode initialisation and ask it to ignore checks
-	copter.mode_rtl.init(true);
+    // call regular rtl flight mode initialisation and ask it to ignore checks
+    copter.mode_rtl.init(true);
 }
 
 // auto_takeoff_start - initialises waypoint controller to implement take-off
 void ModeStar::takeoff_start(const Location& dest_loc)
 {
-	fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
-	_mode = Auto_TakeOff;
+    fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
+    _mode = Auto_TakeOff;
 
-	Location dest(dest_loc);
+    Location dest(dest_loc);
 
-	if (!copter.current_loc.initialised()) {
-		// vehicle doesn't know where it is ATM.  We should not
-		// initialise our takeoff destination without knowing this!
-		return;
-	}
-	fprintf(stderr,"[%s:%d] Start taking off...\n", __FUNCTION__, __LINE__);
+    if (!copter.current_loc.initialised()) {
+        // vehicle doesn't know where it is ATM.  We should not
+        // initialise our takeoff destination without knowing this!
+        return;
+    }
+    fprintf(stderr,"[%s:%d] Start taking off...\n", __FUNCTION__, __LINE__);
 
-	// set horizontal target
-	dest.lat = copter.current_loc.lat;
-	dest.lng = copter.current_loc.lng;
+    // set horizontal target
+    dest.lat = copter.current_loc.lat;
+    dest.lng = copter.current_loc.lng;
 
-	// get altitude target
-	int32_t alt_target;
-	if (!dest.get_alt_cm(Location::AltFrame::ABOVE_HOME, alt_target)) {
-		// this failure could only happen if take-off alt was specified as an alt-above terrain and we have no terrain data
-		AP::logger().Write_Error(LogErrorSubsystem::TERRAIN, LogErrorCode::MISSING_TERRAIN_DATA);
-		// fall back to altitude above current altitude
-		alt_target = copter.current_loc.alt + dest.alt;
-	}
+    // get altitude target
+    int32_t alt_target;
+    if (!dest.get_alt_cm(Location::AltFrame::ABOVE_HOME, alt_target)) {
+        // this failure could only happen if take-off alt was specified as an alt-above terrain and we have no terrain data
+        AP::logger().Write_Error(LogErrorSubsystem::TERRAIN, LogErrorCode::MISSING_TERRAIN_DATA);
+        // fall back to altitude above current altitude
+        alt_target = copter.current_loc.alt + dest.alt;
+    }
 
-	// sanity check target
-	if (alt_target < copter.current_loc.alt) {
-		dest.set_alt_cm(copter.current_loc.alt, Location::AltFrame::ABOVE_HOME);
-	}
+    // sanity check target
+    if (alt_target < copter.current_loc.alt) {
+        dest.set_alt_cm(copter.current_loc.alt, Location::AltFrame::ABOVE_HOME);
+    }
 
-	// Note: if taking off from below home this could cause a climb to an unexpectedly high altitude
-	if (alt_target < 100) {
-		dest.set_alt_cm(100, Location::AltFrame::ABOVE_HOME);
-	}
+    // Note: if taking off from below home this could cause a climb to an unexpectedly high altitude
+    if (alt_target < 100) {
+        dest.set_alt_cm(100, Location::AltFrame::ABOVE_HOME);
+    }
 
-	// set waypoint controller target
-	if (!wp_nav->set_wp_destination(dest)) {
-		// failure to set destination can only be because of missing terrain data
-		copter.failsafe_terrain_on_event();
-		return;
-	}
+    // set waypoint controller target
+    if (!wp_nav->set_wp_destination(dest)) {
+        // failure to set destination can only be because of missing terrain data
+        copter.failsafe_terrain_on_event();
+        return;
+    }
 
-	// initialise yaw
-	auto_yaw.set_mode(AUTO_YAW_HOLD);
+    // initialise yaw
+    auto_yaw.set_mode(AUTO_YAW_HOLD);
 
-	// clear i term when we're taking off
-	set_throttle_takeoff();
+    // clear i term when we're taking off
+    set_throttle_takeoff();
 
-	// get initial alt for WP_NAVALT_MIN
-	auto_takeoff_set_start_alt();
+    // get initial alt for WP_NAVALT_MIN
+    auto_takeoff_set_start_alt();
 }
 
 // auto_wp_start - initialises waypoint controller to implement flying to a particular destination
 void ModeStar::wp_start(const Location& dest_loc)
 {
-	_mode = Auto_WP;
+    _mode = Auto_WP;
 
-	// send target to waypoint controller
-	if (!wp_nav->set_wp_destination(dest_loc)) {
-		// failure to set destination can only be because of missing terrain data
-		copter.failsafe_terrain_on_event();
-		return;
-	}
+    // send target to waypoint controller
+    if (!wp_nav->set_wp_destination(dest_loc)) {
+        // failure to set destination can only be because of missing terrain data
+        copter.failsafe_terrain_on_event();
+        return;
+    }
 
-	// initialise yaw
-	// To-Do: reset the yaw only when the previous navigation command is not a WP.  this would allow removing the special check for ROI
-	if (auto_yaw.mode() != AUTO_YAW_ROI) {
-		auto_yaw.set_mode_to_default(false);
-	}
+    // initialise yaw
+    // To-Do: reset the yaw only when the previous navigation command is not a WP.  this would allow removing the special check for ROI
+    if (auto_yaw.mode() != AUTO_YAW_ROI) {
+        auto_yaw.set_mode_to_default(false);
+    }
 }
 
 // auto_takeoff_run - takeoff in auto mode
 //      called by auto_run at 100hz or more
 void ModeStar::takeoff_run()
 {
-	auto_takeoff_run();
-	if (wp_nav->reached_wp_destination()) {
-		fprintf(stderr,"[%s:%d] Reached Starting point.\n", __FUNCTION__, __LINE__);
-		//const Vector3f target = wp_nav->get_wp_destination();
-		//wp_start(target);
-		_mode = Auto_WP;
-	}
+    auto_takeoff_run();
+    if (wp_nav->reached_wp_destination()) {
+        fprintf(stderr,"[%s:%d] Reached Starting point.\n", __FUNCTION__, __LINE__);
+        _mode = Auto_WP;
+    }
 }
 
 // auto_wp_run - runs the auto waypoint controller
 //      called by auto_run at 100hz or more
 void ModeStar::wp_run()
 {
-	//fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
+    //fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
 
-	if (wp_nav->reached_wp_destination()) {
-		fprintf(stderr,"[%s:%d] Reached Dest# %d.\n",
-				__FUNCTION__, __LINE__, curDest);
+    if (wp_nav->reached_wp_destination()) {
+        fprintf(stderr,"[%s:%d] Reached Dest# %d.\n",
+                __FUNCTION__, __LINE__, curDest);
 
-		Vector3f newOrigin = wp_nav->get_wp_destination();
-		Vector3f newDest;
-		if (++curDest < numDest) {
-			if (!wp_nav->set_wp_destination(wPnts[curDest])) {
-				fprintf(stderr,"[%s:%d] Failed to set a new WP Dest# %d.\n",
-						__FUNCTION__, __LINE__, curDest);
+        if (++curDest < numDest) {
+            wp_nav->shift_wp_origin_to_current_pos();
+            if (!wp_nav->set_wp_destination(wPnts[curDest])) {
+                fprintf(stderr,"[%s:%d] Failed to set a new WP Dest# %d.\n",
+                        __FUNCTION__, __LINE__, curDest);
 
-				// ToDo
-				return;
-			}
-			newDest = wp_nav->get_wp_destination();
-			if (!wp_nav->set_wp_origin_and_destination(newOrigin, newDest)) {
-				fprintf(stderr,"[%s:%d] Failed to set a new WP Origin# %d.\n",
-						__FUNCTION__, __LINE__, curDest);
-				return;
-			}
+                // ToDo
+                return;
+            }
+            Vector3f newOrigin = wp_nav->get_wp_origin();
+            Vector3f newDest = wp_nav->get_wp_destination();
+            fprintf(stderr,"[%s:%d] Next: (%f, %f, %f) -> (%f, %f, %f)\n",
+                    __FUNCTION__, __LINE__, newOrigin.x, newOrigin.y, newOrigin.z,
+                    newDest.x, newDest.y, newDest.z);
+        } else {
+            fprintf(stderr,"[%s:%d] Mission compleleted !!\n",
+                    __FUNCTION__, __LINE__);
+            _mode = Auto_Loiter;
 
-			fprintf(stderr,"[%s:%d] Next: (%f, %f, %f) -> (%f, %f, %f)\n",
-					__FUNCTION__, __LINE__, newOrigin.x, newOrigin.y, newOrigin.z,
-					newDest.x, newDest.y, newDest.z);
-		} else {
-			if (!wp_nav->set_wp_destination(startLoc)) {
-				fprintf(stderr,"[%s:%d] Failed to set a new WP Dest# %d.\n",
-						__FUNCTION__, __LINE__, curDest);
+            return;
+        }
+    }
 
-				// ToDo
-				return;
-			}
-			newDest = wp_nav->get_wp_destination();
-			wp_nav->set_wp_origin_and_destination(newOrigin, newDest);
-			completed = true;
-			fprintf(stderr,"[%s:%d] Mission compleleted !!\n",
-					__FUNCTION__, __LINE__);
-			_mode = Auto_Loiter;
+    // process pilot's yaw input
+    float target_yaw_rate = 0;
+    if (!copter.failsafe.radio) {
+        // get pilot's desired yaw rate
+        target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
+        if (!is_zero(target_yaw_rate)) {
+            auto_yaw.set_mode(AUTO_YAW_HOLD);
+        }
+    }
 
-			return;
-		}
-	}
+    // if not armed set throttle to zero and exit immediately
+    if (is_disarmed_or_landed()) {
+        make_safe_spool_down();
+        wp_nav->wp_and_spline_init();
+        return;
+    }
 
-	// process pilot's yaw input
-	float target_yaw_rate = 0;
-	if (!copter.failsafe.radio) {
-		// get pilot's desired yaw rate
-		target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
-		if (!is_zero(target_yaw_rate)) {
-			auto_yaw.set_mode(AUTO_YAW_HOLD);
-		}
-	}
+    // set motors to full range
+    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
 
-	// if not armed set throttle to zero and exit immediately
-	if (is_disarmed_or_landed()) {
-		make_safe_spool_down();
-		wp_nav->wp_and_spline_init();
-		return;
-	}
+    // run waypoint controller
+    copter.failsafe_terrain_set_status(wp_nav->update_wpnav());
 
-	// set motors to full range
-	motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+    // call z-axis position controller (wpnav should have already updated it's alt target)
+    pos_control->update_z_controller();
 
-	// run waypoint controller
-	copter.failsafe_terrain_set_status(wp_nav->update_wpnav());
-
-	// call z-axis position controller (wpnav should have already updated it's alt target)
-	pos_control->update_z_controller();
-
-	// call attitude controller
-	if (auto_yaw.mode() == AUTO_YAW_HOLD) {
-		// roll & pitch from waypoint controller, yaw rate from pilot
-		attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), target_yaw_rate);
-	} else {
-		// roll, pitch from waypoint controller, yaw heading from auto_heading()
-		attitude_control->input_euler_angle_roll_pitch_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), auto_yaw.yaw(), true);
-	}
+    // call attitude controller
+    if (auto_yaw.mode() == AUTO_YAW_HOLD) {
+        // roll & pitch from waypoint controller, yaw rate from pilot
+        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), target_yaw_rate);
+    } else {
+        // roll, pitch from waypoint controller, yaw heading from auto_heading()
+        attitude_control->input_euler_angle_roll_pitch_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), auto_yaw.yaw(), true);
+    }
 }
 
 void ModeStar::land_run()
@@ -405,28 +387,28 @@ void ModeStar::rtl_run()
 //      called by auto_run at 100hz or more
 void ModeStar::loiter_run()
 {
-	//fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
-	// if not armed set throttle to zero and exit immediately
-	if (is_disarmed_or_landed()) {
-		make_safe_spool_down();
-		wp_nav->wp_and_spline_init();
-		return;
-	}
+    //fprintf(stderr,"[%s:%d] Entering...\n", __FUNCTION__, __LINE__);
+    // if not armed set throttle to zero and exit immediately
+    if (is_disarmed_or_landed()) {
+        make_safe_spool_down();
+        wp_nav->wp_and_spline_init();
+        return;
+    }
 
-	// accept pilot input of yaw
-	float target_yaw_rate = 0;
-	if (!copter.failsafe.radio) {
-		target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
-	}
+    // accept pilot input of yaw
+    float target_yaw_rate = 0;
+    if (!copter.failsafe.radio) {
+        target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
+    }
 
-	// set motors to full range
-	motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+    // set motors to full range
+    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
 
-	// run waypoint and z-axis position controller
-	copter.failsafe_terrain_set_status(wp_nav->update_wpnav());
+    // run waypoint and z-axis position controller
+    copter.failsafe_terrain_set_status(wp_nav->update_wpnav());
 
-	pos_control->update_z_controller();
-	attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), target_yaw_rate);
+    pos_control->update_z_controller();
+    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), target_yaw_rate);
 }
 
 #endif // MODE_STAR_ENABLED == ENABLED
